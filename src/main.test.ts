7f644543055898d353bcd96eec973214fadb41b1
matt-halliday <bat@oksu.re>
2021-02-01 19:22:38 +0000

    100% coverage (no mercy)

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,7 +1,10 @@
 import * as Main from './main';
 
-describe('main.ts', () => {
-    test('it has an export', () => {
+describe('some file to test', () => {
+    test('it has a nonsense function', () => {
         expect(Main.foo).toBeDefined();
+    });
+    test('nonsense function gives what it gets', () => {
+        expect(Main.foo('compliments')).toEqual('compliments');
     });
 });
